308e353317b571df93f1be2c07ea3a7fcde328d3
Bomin Kim <iambxmk@gmail.com>
2022-12-24 18:06:45 +0900

    typescript, eslint, prettier 세팅

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'plugin:prettier/recommended',
+    'react-app',
+    'eslint:recommended',
+    'plugin:react/recommended',
+  ],
+  plugins: ['unused-imports', 'simple-import-sort'],
+  rules: {
+    'prettier/prettier': ['warn', { endOfLine: 'auto' }],
+    'unused-imports/no-unused-imports': 'warn',
+    'simple-import-sort/imports': 'warn',
+    'simple-import-sort/exports': 'warn',
+    'no-unused-vars': 'warn',
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/no-unescaped-entities': 'off', // 문자열 내부 ' " 허용
+  },
+};
