d452bb06a930f3dfd4d377d75c3a43d80d797d2f
Bomin Kim <iambxmk@gmail.com>
2022-12-24 17:13:05 +0900

    Layout Body 생성만

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,13 @@
+import * as React from "react"
+import Body from "../Body"
+// import { Link } from "gatsby"
+
+const Layout = ({ location, title, children }) => {
+  return (
+    <div>
+      <Body />
+    </div>
+  )
+}
+
+export default Layout
