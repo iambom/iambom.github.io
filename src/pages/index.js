ca8737a3e6b979a740a1b4f6acdd7941a8c36945
Bomin Kim <iambxmk@gmail.com>
2022-12-24 16:34:35 +0900

    package.json 수정

--- src/pages/index.js
+++ src/pages/index.js
@@ -68,7 +68,7 @@ export default BlogIndex
  *
  * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
  */
-export const Head = () => <Seo title="B Blog" />
+export const Head = () => <Seo title="iambom.io" />
 
 export const pageQuery = graphql`
   {
